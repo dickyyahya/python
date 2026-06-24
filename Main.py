ac94983934a6ae11fd843f983ab32969c9376972
dickyyahya <dickyyahya25@gmail.com>
2026-06-24 14:14:48 +0700

    membuat compile python

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
+import time
+start_time = time.time()
+
 print ("Hello World!")
 print (9)
 print(True)
+for i in range (1,1000):
+    a=10
+print(time.time()-start_time,"detik")
+
+# compile python ke bytecode
+# terminal python -m py_compile main.py
+# cd __pycache__ trs run python main + tab
