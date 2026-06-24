c91401db063d06cb4dd009e9b64687291f0b2dfd
dickyyahya <dickyyahya25@gmail.com>
2026-06-24 17:50:42 +0700

    separate file variabel

--- Main.py
+++ Main.py
@@ -12,23 +12,4 @@ print(time.time()-start_time,'detik')
 # terminal python -m py_compile main.py
 # cd __pycache__ trs run python main + tab
 
-############################################
-#variabel adalah tempat menyimpan data
-
-#assigment Nilai
-a = 10
-b = 50
-hewan = 'Burung'
-jumlahBurung = 5
-
-#pemanggilan pertama
-print ('Saya mempunyai', hewan ,'sebanyak', jumlahBurung)
-
-#penamaan
-nilai_y = 70
-juta50 = 50000000
-nilaiZ = 20000
-
-#pemanggilan kedua
-print ("Berapa nilai y", nilai_y, "dan", "Berapa harga sawit 1 kuintal",juta50,"Berapa harga sate 1 kodi",nilaiZ)
 
---
+++ variabel.py
@@ -0,0 +1,19 @@
+#variabel adalah tempat menyimpan data
+
+#assigment Nilai
+a = 10
+b = 50
+hewan = 'Burung'
+jumlahBurung = 5
+
+#pemanggilan pertama
+print ('Saya mempunyai', hewan ,'sebanyak', jumlahBurung)
+
+#penamaan
+nilai_y = 70
+juta50 = 50000000
+nilaiZ = 20000
+
+#pemanggilan kedua
+print ("Berapa nilai y", nilai_y, "dan", "Berapa harga sawit 1 kuintal",juta50,"Berapa harga sate 1 kodi",nilaiZ)
+
