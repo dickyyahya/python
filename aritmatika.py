36a7b771a2d747c657c5ef66039c56f46f2e3332
dickyyahya <dickyyahya25@gmail.com>
2026-06-26 06:05:08 +0700

    practice aritmatika

---
+++ aritmatika.py
@@ -0,0 +1,44 @@
+# aritmatika
+a = 11
+b = 5
+
+# operasi penjumlahan (+)
+hasil = a + b
+print(a,"+",b,"=",hasil)
+# operasi pengurangan (-)
+hasil = a - b
+print(a,"-",b,"=",hasil)
+# operasi pembagian (/)
+hasil = a / b
+print(a,"/",b,"=",hasil)
+# operasi perkalian (*)
+hasil = a * b
+print (a,"*",b,"=",hasil)
+# operasi eksponen (pangkat) **
+hasil = a ** b
+print(a,"**",b,"=",hasil)
+#operasi modulus %
+hasil = a % b
+print(a,"%",b,"=",hasil)
+#operasi floor division //
+hasil = a // b
+print (a, "//", b, "=", hasil)
+
+#prioritas operasi
+'''
+urutan pengerjaan 
+1. ()
+2. eksponen **
+3. * / % //
+4. + -
+'''
+
+x = 3
+y = 2
+z = 4
+
+hasil = x + y * z
+print(x,"+",y,"*",z,"=",hasil)
+hasil = (x + y) * z
+print ("(",x,"+",y,")","*",z,"=",hasil)
+
