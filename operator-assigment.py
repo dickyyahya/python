b6c8d97854cf88f52b6969c2d3337852fa6f2fa8
dickyyahya <dickyyahya25@gmail.com>
2026-06-29 10:12:51 +0700

    practice operator assigment

---
+++ operator-assigment.py
@@ -0,0 +1,77 @@
+# operasi yang dapat dilakukan dengan penyingkatan
+# oprasi dengan assigment
+
+a = 7 # adalah assigment
+print ("nilai a =",a)
+
+a += 7  # artinya a = a+7
+print ("nilai a += 7,nilai a menjadi ",a)
+
+a -= 3  # artinya a = a-3
+print ("nilai a -= 3,nilai a menjadi ",a)
+
+a *= 2  # artinya a = a*2
+print ("nilai a *= 2,nilai a menjadi ",a)
+
+a /= 2  # artinya a = a/2
+print ("nilai a /= 2,nilai a menjadi ",a)
+
+a %= 2  # artinya a = a%2
+print ("nilai a /= 2,nilai a menjadi ",a)
+
+b = 10
+b //=3
+print ("nilai b //= 3,nilai b menjadi ",b)
+
+c = 4
+c **=2
+print ("nilai b **= 2,nilai c menjadi ",c)
+
+# operasi bitwise
+# or
+d = True
+print("nilai d =",d)
+d |= False
+print ("nilai d |= False,nilai d menjadi ",d)
+
+d = False
+print("nilai d =",d)
+d |= False
+print ("nilai d |= False,nilai d menjadi ",d)
+
+# and
+d = True
+print("nilai d =",d)
+d &= False
+print ("nilai d &= false,nilai d menjadi ",d)
+
+d = True
+print("nilai d =",d)
+d &= True
+print ("nilai d &= true,nilai d menjadi ",d)
+
+# xor
+d = True
+print("nilai d =",d)
+d ^= False
+print ("nilai d ^= false,nilai d menjadi ",d)
+
+d = True
+print("nilai d =",d)
+d ^= True
+print ("nilai d ^= true,nilai d menjadi ",d)
+
+# geser geser
+e = 0b0100
+print("\nnilai e =",format(e,'04b'))
+e >>= 2
+print ("nilai e >>= 2,nilai e menjadi ",format(e,"04b"))
+
+e <<= 1
+print ("nilai e <<= 1,nilai e menjadi ",format(e,"04b"))
+
+# practice
+
+
+
+
