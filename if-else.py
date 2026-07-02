3d5e5750b07d936ee922e707c44c4cf120350e76
dickyyahya <dickyyahya25@gmail.com>
2026-07-02 16:11:36 +0700

    practice if else

---
+++ if-else.py
@@ -0,0 +1,23 @@
+# if dan else statement
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("masukan nama : ")
+# # if kondisi: aksi
+# 1. progmram inline
+# if nama == "budi" : print( "welcome")
+# print("kowen sapa {nama}")
+
+#2. program if identitation
+# if nama == "budi" :
+#      print( "welcome")
+# print(f"kowen sapa {nama}")
+
+# 3. else statement
+if nama == "budi" :
+     print( "welcome")
+else:
+     print("kowen sapa???")
+print(f"tq ws join {nama}")
+
