8a291f743b74b0c150b66702b9e09b09e509f4df
dickyyahya <dickyyahya25@gmail.com>
2026-07-02 16:46:49 +0700

    practice else if

---
+++ else-if.py
@@ -0,0 +1,13 @@
+# elif = else if statement
+
+nama = input ("masukan nama")
+
+if nama == "budi":
+    print(f"halo {nama} ")
+elif nama == "yanto":
+    print(f"halo {nama}")
+elif nama == "agus":
+    print(f"halo {nama}")
+else:
+    print("kowen sapa")
+print (f"welcome {nama}")
